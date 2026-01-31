21b902c883219f950074ed08f54f92a046f35f07
Harshit <219007351+harshit-066-ui@users.noreply.github.com>
2026-01-31 13:47:30 +0530

    Change database password to a placeholder
    Updated database password to a placeholder for security.

--- task_tracker/backend/db.js
+++ task_tracker/backend/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "K1@1y1d1e1n",
+  password: process.env.DB_PASSWORD || "####",
   database: process.env.DB_NAME || "nexmora_db",
   waitForConnections: true,
   connectionLimit: 10
@@ -18,3 +18,4 @@ db.getConnection((err, connection) => {
     connection.release();
   }
 });
+
