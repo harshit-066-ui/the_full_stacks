7b6a936aa444bc45bfb841be808b7cdecfce46be
Harshit <219007351+harshit-066-ui@users.noreply.github.com>
2026-02-06 18:02:33 +0530

    Add files via upload

---
+++ blogpost/backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import postRoutes from "./routes/postRoutes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/posts", postRoutes);
+
+export default app;
